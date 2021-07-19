d81c555ac3b412f644d6b3e1beae8fa3621d6f87
Pere Viader <pere.viader22@gmail.com>
2021-07-19 10:17:15 +0200

    Preparing unity project for the development

---
+++ Assets/ManualDi/Runtime/TypeBindingGameObjectExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace ManualDi.Unity3d
+{
+    public static class TypeBindingGameObjectExtensions
+    {
+    }
+}
---
+++ Assets/ManualDi/Tests/TestTypeBindingGameObjectExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ManualDi.Unity3d.Tests
+{
+    public class TestTypeBindingGameObjectExtensions
+    {
+        public void TestTestMethod()
+        {
+
+        }
+    }
+}
