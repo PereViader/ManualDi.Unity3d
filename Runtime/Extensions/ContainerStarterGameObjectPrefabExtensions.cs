95142058ff6c853e6848a46d0967673acab24683
Pere Viader <pere.viader22@gmail.com>
2021-07-28 11:56:52 +0200

    First pass on unity functionality for manual di

---
+++ Runtime/Extensions/ContainerStarterGameObjectPrefabExtensions.cs
@@ -0,0 +1,25 @@
+﻿using ManualDi.Main;
+
+using UnityEngine;
+
+namespace Odyssey.Game.Simulation
+{
+    public static class ContainerStarterGameObjectPrefabExtensions
+    {
+        public static IContainerStarter WithGameObjectPrefabInstaller<T>(this IContainerStarter containerStarter, T monoBehaviourInstallerPrefab, Transform parentTransform) where T : MonoBehaviourInstaller
+        {
+            containerStarter.WithDelegatedInstaller((IDiContainer container) =>
+            {
+                T monoBehaviourInstaller = GameObject.Instantiate(
+                    monoBehaviourInstallerPrefab,
+                    parentTransform
+                    );
+
+                container.QueueDispose(() => GameObject.Destroy(monoBehaviourInstaller.gameObject));
+
+                return monoBehaviourInstaller;
+            });
+            return containerStarter;
+        }
+    }
+}
